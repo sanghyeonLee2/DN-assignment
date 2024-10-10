4f508b14d592645f1e9df36d38328a241749dd22
sanghyeonLee <hyeon012366@gmail.com>
2024-10-10 20:30:08 +0900

    1부터 4096 출력 완료

---
+++ src/4096.c
@@ -0,0 +1,17 @@
+//# include <stdio.h>
+//
+//
+//int main() {
+//    for (int i = 0; i <= 7; i++) {
+//        for (int j = 0; j <= 7; j++) {
+//            for (int u = 0; u <= 7; u++) {
+//                for (int k = 1; k <= 8; k++) {
+//                    printf("%4d", (u * 64) + (j * 8) + k + (i * 512));
+//                    printf("");
+//                }
+//
+//            }printf("\n");
+//        }
+//    }
+//    return 0;
+//}
